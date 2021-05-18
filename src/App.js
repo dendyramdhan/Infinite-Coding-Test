2d3013d2515f458678179ae4176b96c9c7eeb5c6
Dendy Ramdhan <ramdhandendy@gmail.com>
2021-05-18 09:52:14 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [number, setNumber] = useState(null);
+  const [numbers, setNumbers] = useState([]);
+
+  const onChange = (e) => {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+      setNumber(e.target.value)
+    }
+  }
+
+  const onSubmit = () => {
+    if (number) {
+      let result = [];  
+
+      for (let i = 1; i <= number; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+          result.push('FizzBuzz');
+        } else if (i % 3 == 0) {
+          result.push('Fizz');
+        } else if (i % 5 == 0) {
+          result.push('Buzz');
+        } else {
+          result.push(i);        
+        }
+      }
+
+      setNumbers(result)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" value={number} onChange={onChange} />
+        <button onClick={onSubmit}>Submit</button>
+      </div>
+      <div className="numbers">
+        {numbers.length > 0 && numbers.map(v => (
+          <div>
+            {v}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
